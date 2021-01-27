76e8f7737152995681ef1e11911a8e62abf361e9
Hamdy Dawood <hamdykhalid12345@gmail.com>
2021-01-28 01:10:48 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,8 @@ class BMI extends StatefulWidget {
 class _State extends State<BMI> {
 Gender g;
 double height=150;
+int Age=19;
+int Weight=70;
   @override
   Widget build(BuildContext context) {
 
@@ -30,8 +32,10 @@ double height=150;
           SizedBox(height: 10,),
           Row(
             children: [
-           Expanded(
-             child: Padding(
+
+
+
+             Padding(
                padding: const EdgeInsets.all(8.0),
                child:GestureDetector(onTap: (){
                  setState(() {
@@ -39,25 +43,27 @@ double height=150;
                  });
                },
                child: Container(
+                 height: 190,
+                 width: 165,
                 decoration:BoxDecoration(
                   color: g==Gender.Male?active:inactive,
                   borderRadius:BorderRadius.circular(30)
                 ),
                 child: Column(
                   children: [
-                    Text('Male',style: TextStyle(color: Colors.white,fontSize: 25),),
-                    Icon(FontAwesomeIcons.venus,color: Colors.white,size: 90,)
+                    SizedBox(height: 10,),
+                    Text('Male',style: TextStyle(color: Colors.white,fontSize: 30),),
+                    SizedBox(height: 20,),
+                    Icon(FontAwesomeIcons.venus,color: Colors.white,size: 100,)
                   ],
                 ),
 
               ),
                )
-             ),
+
 
            ),
-              Expanded(
-
-                child: Padding(
+            Padding(
                   padding: const EdgeInsets.all(8.0),
                   child:GestureDetector(onTap: (){
                     setState(() {
@@ -65,14 +71,18 @@ double height=150;
                     });
                      },
                    child: Container(
+                     height: 190,
+                     width: 160,
                     decoration:BoxDecoration(
                         color: g==Gender.Famale?active:inactive,
                         borderRadius:BorderRadius.circular(30)
                     ),
                     child: Column(
                       children: [
-                        Text('Male',style: TextStyle(color: Colors.white,fontSize: 25),),
-                        Icon(FontAwesomeIcons.mars,color: Colors.white,size: 90,)
+                        SizedBox(height: 10,),
+                        Text('Male',style: TextStyle(color: Colors.white,fontSize: 30),),
+                        SizedBox(height: 20,),
+                        Icon(FontAwesomeIcons.mars,color: Colors.white,size: 100,)
                       ],
                     ),
 
@@ -80,44 +90,148 @@ double height=150;
                   ),
                 ),
 
-              )
+
+
 
             ],
 
           ),
-          Expanded(
-            
-            child: Container(
+
+           Container(
+             height: 180,width: 345,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(30),
                 color:active,
               ),
               child:Column(
                 children: [
+                  SizedBox(height: 8,),
                   Text('Height',style:TextStyle(fontSize: 30,color: Colors.grey),),
-                  Text(height.toString(),style:TextStyle(fontSize: 40,color: Colors.white),),
-                Slider(max: 250, min: 50,activeColor:Colors.pink,inactiveColor:Colors.grey,
+                  SizedBox(height: 10,),
+                  Text(height.toStringAsFixed(1),style:TextStyle(fontSize: 40,color: Colors.white),),
+                  SizedBox(height: 8,),
+                Slider(max: 250, min: 100,activeColor:Colors.pink,inactiveColor:Colors.grey,
                 value: height ,onChanged: (value){
+                setState(() {
                   height=value;
+                });
                   },),
 
 
                 ],
               ),
-            ),
+
           ),
+
           Row(
             children: [
-              Container(
-                child: Column(
-                  children: [
-                    Text('Age',style:TextStyle(fontSize: 30,color: Colors.grey),)
-                  ],
+
+
+                 Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                       height: 190,
+                      width: 160,
+                      decoration: BoxDecoration(
+                       borderRadius: BorderRadius.circular(30),
+                       color:active,
+                       ),
+                       child: Column(
+                        children: [
+                          SizedBox(height: 10,),
+                          Text('Age',style:TextStyle(fontSize: 30,color: Colors.grey),),
+                          SizedBox(height: 10,),
+                          Text(Age.toString(),style:TextStyle(fontSize: 40,color: Colors.white),),
+                        SizedBox(height: 10,),
+
+                         Row(
+                          children: [
+                            SizedBox(width: 35,),
+                            GestureDetector(onTap: (){
+                              setState(() {
+                                Age++;
+                              });},
+                              child:Icon(Icons.add_circle,color: Colors.grey,size: 40,) ,),
+                            SizedBox(width: 25,),
+                            GestureDetector(onTap: (){
+                              setState(() {
+                                Age--;
+                              });},
+                              child:Icon(Icons.remove_circle,color: Colors.grey,size: 40,) ,),
+                                SizedBox(width: 10,)
+                          ],
+                        ),
+                           SizedBox(height: 10,)
+
+
+
+                        ],
+
+
+               ),
                 ),
+                   ),
+
+
+               Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 190,
+                  width: 165,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color:active,
+                  ),
+                   child: Column(
+                    children: [
+                      SizedBox(height: 10,),
+                      Text('Weight',style:TextStyle(fontSize: 30,color: Colors.grey),),
+                      SizedBox(height: 10,),
+                      Text(Weight.toString(),style:TextStyle(fontSize: 40,color: Colors.white),),
+                      SizedBox(height: 10,),
+
+                      Row(
+                        children: [
+                          SizedBox(width: 35,),
+                          GestureDetector(onTap: (){
+                            setState(() {
+                              Weight++;
+                            });},
+                            child:Icon(Icons.add_circle,color: Colors.grey,size: 40,) ,),
+                          SizedBox(width: 25,),
+                          GestureDetector(onTap: (){
+                            setState(() {
+                              Weight--;
+                            });},
+                            child:Icon(Icons.remove_circle,color: Colors.grey,size: 40,) ,),
+                          SizedBox(width: 10,)
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+
+
+                    ],
+
+                  ),
+
+                )
               )
+
             ],
+          ),
+          SizedBox(height: 10,),
+          Container(
+            height: 45,width: 320,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color:Colors.pink,
+
+            ),
+
+            child: Center(child: Text('CALCULATE BMI',style: TextStyle(color: Colors.black,fontSize: 20),)),
           )
         ],
+
       ),
     )
     );
